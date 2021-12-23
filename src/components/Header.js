0c5029b1c53dbe185c49785c9ca106e516542114
Viswanathan TJ <viswanathantj@gmail.com>
2021-12-23 18:30:30 +0530

    Application build done

---
+++ src/components/Header.js
@@ -0,0 +1,28 @@
+import PropTypes from 'prop-types';
+import Button from './Button';
+// rafce
+const Header = ({ title, onAdd, showAdd }) => {
+
+    const onClick = () => {
+        console.log('click')
+    }
+
+    return  (
+        <header className='header'>
+            <h1>{title}</h1>
+            <Button color={showAdd ? 'red' : 'green'}
+                text={showAdd ? 'Close' : 'Add'}
+                onClick={onAdd} />
+        </header>
+    )
+}
+
+Header.defaultProps = {
+    title: 'Task Tracker',
+}
+
+Header.propTypes = {
+    title: PropTypes.string.isRequired,
+}
+
+export default Header
